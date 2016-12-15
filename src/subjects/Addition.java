6b9578c8625722692e18a6d4e43a67f1d463bee8
shoshen <shoshen11@gmail.com>
2016-12-14 20:34:35 -0500

    adding this file to the repo

---
+++ src/subjects/Addition.java
@@ -0,0 +1,9 @@
+package subjects;
+
+public class Addition {
+	
+	public void add(int a, int b){
+		System.out.println(a+b);
+	}
+	
+}
---
+++ src/subjects/Test.java
@@ -0,0 +1,10 @@
+package subjects;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Addition addition = new Addition();
+		addition.add(43,45);
+	}
+
+}
